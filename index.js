4c2430d04e7021deae0d895aef7cd9796f1d1e27
HannerB <118286518+HannerB@users.noreply.github.com>
2024-05-14 11:14:29 -0500

    Fix - Tiempo de espera y construcción de arrays

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ async function openWebPage(){
 
 async function getDataFromWebPage(){
     const browser = await puppeteer.launch({
-        headless: true, // Modificado a true para que no se abra el navegador
-        executablePath: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe', // Ruta al ejecutable de Microsoft Edge
-        slowMo:70
+        headless: true,
+        // slowMo:70
     })
     const page  = await  browser.newPage()
 
-    // Desactivar la carga de imágenes y hojas de estilo
     await page.setRequestInterception(true);
     page.on('request', (req) => {
         if(req.resourceType() == 'image' || req.resourceType() == 'stylesheet'){
@@ -31,23 +29,27 @@ async function getDataFromWebPage(){
         }
     });
 
-    await page.goto('https://betplay.com.co/apuestas#sports-hub/basketball/nba')
+    try {
+        await page.goto('https://betplay.com.co/apuestas#sports-hub/basketball/nba', {timeout: 90000}); // Timeout of 60 seconds
+    } catch (error) {
+        console.error(error);
+    }
 
-    // Espera a que los elementos específicos aparezcan en la página
     await page.waitForSelector("div.KambiBC-event-participants__name--team-logo")
     await page.waitForSelector('div.eMQclt')
 
     const result = await page.evaluate(() =>{
-        const teamsElement = document.querySelector("div.KambiBC-event-participants__name--team-logo")
-        const quotaElement = document.querySelector('div.eMQclt')
-        const teams = teamsElement ? teamsElement.innerText : 'No se encontró el título'
-        const quota = quotaElement ? quotaElement.innerText : 'No se encontraron los equipos'
-        return {teams, quota} // Devuelve el título y los equipos
+        const teamsElements = document.querySelectorAll("div.KambiBC-event-participants__name--team-logo")
+        const quotaElements = document.querySelectorAll('div.eMQclt')
+        const teams = Array.from(teamsElements).map(element => element.innerText)
+        const quotas = Array.from(quotaElements).map(element => element.innerText)
+        return teams.map((team, i) => ({team, quota: quotas[i]}))
     })
 
-    console.log(result) // Imprime el resultado
+    console.log(result)
 
-    await browser.close() // Cierra el navegador después de obtener los datos
+    await browser.close()
 }
 
 getDataFromWebPage()
+
