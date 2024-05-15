4c2e0c5bd92eadb8b7ddca177340be9b702dff61
HannerB <118286518+HannerB@users.noreply.github.com>
2024-05-15 16:45:41 -0500

    MOD - Nuevas funciones.
    -Se consume la api de forma estable (aumento en el tiempo de búsqueda).
    -Se agregó el nuevo apartado visual que muestra el scrap de otra página.

--- scrap_react/src/ScrapComponent.js
+++ scrap_react/src/ScrapComponent.js
@@ -1,37 +1,42 @@
 import React, { useState } from 'react';
 
 function ScrapComponent() {
-    const [datos, setDatos] = useState(null);
+    const [datosPrimeraPagina, setDatosPrimeraPagina] = useState(null);
+    const [datosSegundaPagina, setDatosSegundaPagina] = useState(null);
     const [buscando, setBuscando] = useState(false);
     const [horaInicio, setHoraInicio] = useState(null);
     const [horaFin, setHoraFin] = useState(null);
 
-    const fetchData = () => {
+    const fetchData = async () => {
         // Establecer los datos en null para borrar los datos antiguos
-        setDatos(null);
+        setDatosPrimeraPagina(null);
+        setDatosSegundaPagina(null);
         // Indicar que se está realizando la búsqueda
         setBuscando(true);
         // Establecer la hora de inicio de la búsqueda
         setHoraInicio(new Date());
 
-        // Realizar la solicitud a la API cuando se active el evento de clic en el botón
-        fetch('http://localhost:3001/datos-web') // Cambia el puerto a 3001
-            .then(response => response.json())
-            .then(data => {
-                // Adaptar los datos recibidos para incluir el nombre del equipo y su quota juntos
-                const formattedData = data.map(item => `${item.team} - ${item.quota}`);
-                setDatos(formattedData);
-            })
-            .catch(error => {
-                // Manejar errores de la solicitud
-                console.error('Ocurrió un error al obtener los datos:', error);
-            })
-            .finally(() => {
-                // Indicar que se ha completado la búsqueda
-                setBuscando(false);
-                // Establecer la hora de finalización de la búsqueda
-                setHoraFin(new Date());
-            });
+        try {
+            // Realizar la solicitud a la API para la primera página
+            const response1 = await fetch('http://localhost:3001/datos-web');
+            const data1 = await response1.json();
+            const formattedData1 = data1.map(item => `${item.team} - ${item.quota}`);
+            setDatosPrimeraPagina(formattedData1);
+
+            // Realizar la solicitud a la API para la segunda página
+            const response2 = await fetch('http://localhost:3001/datos-web-segunda-pagina');
+            const data2 = await response2.json();
+            const formattedData2 = data2.map(item => `${item.team} - ${item.quota}`);
+            setDatosSegundaPagina(formattedData2);
+        } catch (error) {
+            // Manejar errores de la solicitud
+            console.error('Ocurrió un error al obtener los datos:', error);
+        } finally {
+            // Indicar que se ha completado la búsqueda
+            setBuscando(false);
+            // Establecer la hora de finalización de la búsqueda
+            setHoraFin(new Date());
+        }
     };
 
     // Renderizar los datos en tu componente
@@ -43,19 +48,30 @@ function ScrapComponent() {
             {buscando ? (
                 <p>Buscando resultados...</p>
             ) : (
-                datos ? (
-                    <>
-                        <p>Hora de inicio: {horaInicio && horaInicio.toLocaleTimeString()}</p>
-                        <p>Hora de fin: {horaFin && horaFin.toLocaleTimeString()}</p>
-                        <ul>
-                            {datos.map((dato, index) => (
-                                <li key={index}>{dato}</li>
-                            ))}
-                        </ul>
-                    </>
-                ) : (
-                    <p>Presiona el botón para obtener datos...</p>
-                )
+                <>
+                    {datosPrimeraPagina && (
+                        <>
+                            <h2>Datos de la primera página:</h2>
+                            <ul>
+                                {datosPrimeraPagina.map((dato, index) => (
+                                    <li key={index}>{dato}</li>
+                                ))}
+                            </ul>
+                        </>
+                    )}
+                    {datosSegundaPagina && (
+                        <>
+                            <h2>Datos de la segunda página:</h2>
+                            <ul>
+                                {datosSegundaPagina.map((dato, index) => (
+                                    <li key={index}>{dato}</li>
+                                ))}
+                            </ul>
+                        </>
+                    )}
+                    <p>Hora de inicio: {horaInicio && horaInicio.toLocaleTimeString()}</p>
+                    <p>Hora de fin: {horaFin && horaFin.toLocaleTimeString()}</p>
+                </>
             )}
         </div>
     );
